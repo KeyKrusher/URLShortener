1d634fc9654e6356dbc5ccd23bec85f929e1b0ae
Jack Clark <chubz@Jacks-MacBook-Air.local>
2017-05-20 15:42:11 -0500

    Initial Commit

---
+++ vim.py
@@ -0,0 +1,6 @@
+# Code written by Jack Clark
+# Code for URL Shortener
+# Go here for more stuff: https://impythonist.wordpress.com/2015/10/31/building-your-own-url-shortening-service-with-python-and-flask/
+import os
+import sys
+import platform
